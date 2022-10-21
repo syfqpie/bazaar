3fd121aa956ea52b88fb534e7b5527a796971234
Syafiq Basri <altsyafiq@gmail.com>
2022-10-21 21:38:55 +0800

    :zap: Updated get_permissions in CustomUserViewSet

--- api/users/views.py
+++ api/users/views.py
@@ -10,6 +10,8 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 
 from django_filters.rest_framework import DjangoFilterBackend
 
+from utils.auth.permissions import IsSuperAdmin
+
 from .models import (
     UserType,
     CustomUser
@@ -33,6 +35,6 @@ class CustomUserViewSet(viewsets.ModelViewSet):
     ]
 
     def get_permissions(self):
-        permission_classes = [IsAuthenticated]
+        permission_classes = [IsAuthenticated, IsSuperAdmin]
 
         return [permission() for permission in permission_classes]    
