8491d3d2666507df16d0eda29f7e0439c69c8f48
Syafiq Basri <altsyafiq@gmail.com>
2022-10-23 22:13:18 +0800

    :sparkles: Added AuthLayout

---
+++ web/src/layouts/AuthLayout.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="h-screen">
+        <div class="grid grid-cols-6 gap-0 h-full content-center">
+            <div class="col-start-2 col-span-4">
+                <div class="grid grid-cols-1 sm:grid-cols-2 
+                    gap-0 rounded-lg overflow-hidden shadow-xl">
+                    <div class="bg-green-400 hidden sm:block"></div>
+
+                    <div>
+                        <RouterView />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script lang="ts">
+import { onMounted, defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'AuthLayout',
+    setup() {
+        onMounted(() => {
+            // console.log('Mounted AuthLayout')
+        })
+
+        return {}
+    }
+})
+</script>
