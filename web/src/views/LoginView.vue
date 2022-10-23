ea5478a220710d8715cc768ea6ef408203e49a76
Syafiq Basri <altsyafiq@gmail.com>
2022-10-23 22:16:27 +0800

    :sparkles: Added LoginView draft

---
+++ web/src/views/LoginView.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="w-full py-8 px-12">
+        <div>
+            <img alt="Bazaar"
+                class="mx-auto h-12 w-auto"
+                src="@/assets/img/default/trolley.png" />
+
+            <h2 class="mt-6 text-center text-3xl
+                font-bold tracking-tight text-gray-900">
+                Sign in to your account
+            </h2>
+        </div>
+
+        <form>
+            <div class="mt-8 max-w-md">
+                <div class="grid grid-cols-1 gap-3">
+                    <div>
+                        <label class="text-gray-700">Email</label>
+                        <input type="email"
+                            class="mt-1 block w-full rounded-md bg-gray-100
+                            border-transparent px-3 py-2 focus:outline-none
+                            focus:shadow-outline focus:ring-gray-500
+                            focus:ring-1 focus:bg-white"
+                            placeholder="Enter your email" />
+                    </div>
+
+                    <div>
+                        <label class="text-gray-700">Password</label>
+                        <input type="password"
+                            class="mt-1 block w-full rounded-md bg-gray-100
+                            border-transparent px-3 py-2 focus:outline-none
+                            focus:shadow-outline focus:ring-gray-500
+                            focus:ring-1 focus:bg-white"
+                            placeholder="Enter your password" />
+                    </div>
+
+                    <div class="mt-1 flex items-center justify-between">
+                        <div class="flex items-center">
+                            <input id="remember-me"
+                                name="remember-me"
+                                type="checkbox"
+                                class="h-4 w-4 rounded border-gray-300
+                                text-green-600 focus:ring-green-500" />
+                            <label for="remember-me"
+                                class="ml-2 block text-sm text-gray-900">
+                                Remember me
+                            </label>
+                        </div>
+
+                        <div class="text-sm">
+                            <a href="#" 
+                                class="font-medium text-green-400
+                                hover:text-green-300">
+                                Forgot your password?
+                            </a>
+                        </div>
+                    </div>
+
+                    <div>
+                        <button type="submit"
+                            class="mt-1 group relative flex w-full justify-center
+                            rounded-md border border-transparent bg-green-300
+                            px-3 py-2 text-white hover:bg-green-400
+                            focus:outline-none focus:ring-2 focus:ring-green-200">
+                            Sign in
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script lang="ts">
+import { onMounted, defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Login',
+  setup() {
+    onMounted(() => {
+      // console.log('Mounted Login')
+    })
+
+    return {}
+  }
+})
+</script>
