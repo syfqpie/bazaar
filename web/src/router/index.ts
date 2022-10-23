1e6c0f5ad39b98ad392b5b850e8928265e16e8ef
Syafiq Basri <altsyafiq@gmail.com>
2022-10-23 22:17:13 +0800

    :zap: Added auth path to router

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '../views/HomeView.vue'
 import PublicLayoutVue from '@/layouts/PublicLayout.vue'
+import AuthLayoutVue from '@/layouts/AuthLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,12 +20,16 @@ const router = createRouter({
       ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/auth',
+      name: 'authentication',
+      component: AuthLayoutVue,
+      children: [
+        {
+          path: 'login',
+          name: 'login',
+          component: () => import('../views/LoginView.vue')
+        }
+      ]
     }
   ]
 })
