f66036bad9086b45a1eb7060cd783affacfc1fdf
Syafiq Basri <altsyafiq@gmail.com>
2022-10-24 22:58:36 +0800

    :sparkles: Added MyVerifyEmailSerializer, MySetPasswordSerializer; Updated MyVerifyEmailView;

--- api/utils/auth/serializers.py
+++ api/utils/auth/serializers.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.forms import SetPasswordForm
+
 from rest_framework import serializers
 
-from dj_rest_auth.registration.serializers import RegisterSerializer
+from dj_rest_auth.registration.serializers import RegisterSerializer, VerifyEmailSerializer
 from dj_rest_auth.serializers import PasswordResetSerializer
 
 from allauth.account import app_settings
@@ -53,3 +55,42 @@ class MyPasswordResetSerializer(PasswordResetSerializer):
     @property
     def password_reset_form_class(self):
         return MyResetPasswordForm
+
+
+class MyVerifyEmailSerializer(VerifyEmailSerializer):
+    """
+    Custom serializer to for M
+    """
+
+    new_password1 = serializers.CharField(max_length=128)
+    new_password2 = serializers.CharField(max_length=128)
+
+
+class MySetPasswordSerializer(serializers.Serializer):
+    """
+    Custom serializer to enable change password while
+    verifying email
+    """
+
+    new_password1 = serializers.CharField(max_length=128)
+    new_password2 = serializers.CharField(max_length=128)
+
+    set_password_form_class = SetPasswordForm
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.request = self.context.get('request')
+        self.user = self.request['user']
+
+    def validate(self, attrs):
+        self.set_password_form = self.set_password_form_class(
+            user=self.user, data=attrs
+        )
+
+        if not self.set_password_form.is_valid():
+            raise serializers.ValidationError(self.set_password_form.errors)
+        return attrs
+
+    def save(self):
+        self.set_password_form.save()
+    
