b6d9c5c9f51505fd8091553509bec4a2e0155cb7
Syafiq Basri <altsyafiq@gmail.com>
2022-10-24 12:57:02 +0800

    :sparkles: Added new Auth related models and actions

--- web/src/common/models/auth/auth.model.ts
+++ web/src/common/models/auth/auth.model.ts
@@ -7,18 +7,6 @@ export interface LoginInput {
     password: string | null
 }
 
-/** Interface for login response */
-export interface LoginResponse {
-    /** Generated access token */
-    accessToken: string,
-
-    /** Generated refresh token */
-    refreshToken: string,
-
-    /** Short user information */
-    user: AuthUser
-}
-
 /** Interface for auth user */
 export interface AuthUser {
     /** User's database ID */
@@ -32,4 +20,58 @@ export interface AuthUser {
 
     /** User's last name */
     lastName: string
+}
+
+/** Interface for email only input */
+export interface EmailOnly {
+    /** Related email */
+    email: string
+}
+
+/** Interface for token only input */
+export interface TokenOnly {
+    /** Received token */
+    token: string
+}
+
+/** Interface for change password input */
+export interface ChangePassword {
+    /** New password */
+    newPassword1: string,
+
+    /** Confirm new password */
+    newPassword2: string
+}
+
+/** Interface for confirm reset password input */
+export interface ResetPassword {
+    /** New password */
+    newPassword1: string,
+
+    /** Confirm new password */
+    newPassword2: string
+
+    /** Received uid */
+    id: number,
+
+    /** Received token */
+    token: string
+}
+
+/** Interface for login response */
+export interface LoginResponse {
+    /** Generated access token */
+    accessToken: string,
+
+    /** Generated refresh token */
+    refreshToken: string,
+
+    /** Short user information */
+    user: AuthUser
+}
+
+/** Interface for detail response */
+export interface DetailResponse {
+    /** Detail */
+    detail: string
 }
--- web/src/stores/modules/auth.store.ts
+++ web/src/stores/modules/auth.store.ts
@@ -1,5 +1,12 @@
 import { APIService } from '@/common/api'
-import type { LoginInput, LoginResponse } from '@/common/models/auth/auth.model'
+import type {
+    ChangePassword,
+    DetailResponse,
+    EmailOnly,
+    LoginInput,
+    LoginResponse,
+    ResetPassword,
+    TokenOnly } from '@/common/models/auth/auth.model'
 import { defineStore } from 'pinia'
 
 const BASE_PREFIX = 'auth'
@@ -22,6 +29,100 @@ export const useAuthStore = defineStore('auth', {
         login(payload: LoginInput): Promise<LoginResponse> {
             return new Promise((resolve, reject) => {
                 APIService.post(`${ BASE_PREFIX }/login`, payload)
+                    .then(({ data }) => {
+                        resolve(data)
+                    })
+                    .catch(err => {
+                        reject(err)
+                    })
+            })
+        },
+        /**
+         * Resend verification email
+         * 
+         * @param payload - payload
+         * @param payload.email - user's email
+         * @returns Detail message
+         */
+        resendVerification(payload: EmailOnly): Promise<DetailResponse> {
+            return new Promise((resolve, reject) => {
+                APIService.post(`${ BASE_PREFIX }/resend-verification`, payload)
+                    .then(({ data }) => {
+                        resolve(data)
+                    })
+                    .catch(err => {
+                        reject(err)
+                    })
+            })
+        },
+        /**
+         * Resend verification email
+         * 
+         * @param payload - payload
+         * @param payload.token - received token
+         * @returns Detail message
+         */
+        verifyAccount(payload: TokenOnly): Promise<DetailResponse> {
+            return new Promise((resolve, reject) => {
+                APIService.post(`${ BASE_PREFIX }/verify-email`, payload)
+                    .then(({ data }) => {
+                        resolve(data)
+                    })
+                    .catch(err => {
+                        reject(err)
+                    })
+            })
+        },
+        /**
+         * Change account password
+         * 
+         * @param payload - payload
+         * @param payload.newPassword1 - new password
+         * @param payload.newPassword2 - confirm new password
+         * @returns Detail message
+         */
+        changePassword(payload: ChangePassword): Promise<DetailResponse> {
+            return new Promise((resolve, reject) => {
+                APIService.post(`${ BASE_PREFIX }/password/change`, payload)
+                    .then(({ data }) => {
+                        resolve(data)
+                    })
+                    .catch(err => {
+                        reject(err)
+                    })
+            })
+        },
+        /**
+         * Request reset account password
+         * 
+         * @param payload - payload
+         * @param payload.email - user's email
+         * @returns Detail message
+         */
+        requestReset(payload: EmailOnly): Promise<DetailResponse> {
+            return new Promise((resolve, reject) => {
+                APIService.post(`${ BASE_PREFIX }/password/reset`, payload)
+                    .then(({ data }) => {
+                        resolve(data)
+                    })
+                    .catch(err => {
+                        reject(err)
+                    })
+            })
+        },
+        /**
+         * Reset account password
+         * 
+         * @param payload - payload
+         * @param payload.newPassword1 - new password
+         * @param payload.newPassword2 - confirm new password
+         * @param payload.uid - received uid
+         * @param payload.token - received token
+         * @returns Detail message
+         */
+        resetPassword(payload: ResetPassword): Promise<DetailResponse> {
+            return new Promise((resolve, reject) => {
+                APIService.post(`${ BASE_PREFIX }/password/reset/confirm`, payload)
                     .then(({ data }) => {
                         resolve(data)
                     })
