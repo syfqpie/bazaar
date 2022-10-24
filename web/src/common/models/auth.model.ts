1c2c4b4c6338a30ac1e5e52b6e94732d68889a4b
Syafiq Basri <altsyafiq@gmail.com>
2022-10-24 23:31:20 +0800

    :zap: Added RegisterCustomerInput, RegisterVendorInput; Renamed ResetPassword to ResetPasswordInput, ChangePassword to ChangePasswordInput, EmailOnly to EmailOnlyInput; Changed TokenOnly to VerifyAccountInput; Rearranged codes; Changed path

---
+++ web/src/common/models/auth.model.ts
@@ -0,0 +1,101 @@
+/** Interface for change password input */
+export interface ChangePasswordInput {
+    /** New password */
+    newPassword1: string | null,
+
+    /** Confirm new password */
+    newPassword2: string | null
+}
+
+/** Interface for email only input */
+export interface EmailOnlyInput {
+    /** Related email */
+    email: string | null
+}
+
+/** Interface for login input */
+export interface LoginInput {
+    /** Username or email of user */
+    username: string | null,
+
+    /** User's password */
+    password: string | null
+}
+
+/** Interface for register customer input */
+export interface RegisterCustomerInput {
+    /** Related email */
+    username: string | null,
+
+    /** Customer first name */
+    firstName: string | null,
+
+    /** Customer last name */
+    lastName: string | null,
+
+    /** Customer phone number */
+    phoneNo: string | null,
+
+    /** Customer date of birth */
+    dateOfBirth: string | null
+}
+
+/** Interface for register vendor input */
+export interface RegisterVendorInput {
+    /** Related email */
+    username: string | null,
+
+    /** Vendor name */
+    name: string | null,
+
+    /** Vendor phone number */
+    phoneNo: string | null
+}
+
+/** Interface for confirm reset password input */
+export interface ResetPasswordInput extends ChangePasswordInput {
+    /** Received uid */
+    uid: number | null,
+
+    /** Received token */
+    token: string | null
+}
+
+/** Interface for verify account input */
+export interface VerifyAccountInput extends ChangePasswordInput {
+    /** Received token */
+    token: string | null
+}
+
+/** Interface for login response */
+export interface LoginResponse {
+    /** Generated access token */
+    accessToken: string,
+
+    /** Generated refresh token */
+    refreshToken: string,
+
+    /** Short user information */
+    user: AuthUser
+}
+
+/** Interface for detail response */
+export interface DetailResponse {
+    /** Detail */
+    detail: string
+}
+
+/** Interface for auth user */
+export interface AuthUser {
+    /** User's database ID */
+    pk: number,
+
+    /** User's username or password */
+    username: string,
+
+    /** User's first name */
+    firstName: string,
+
+    /** User's last name */
+    lastName: string
+}
