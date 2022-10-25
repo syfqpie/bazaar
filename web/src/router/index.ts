a611795b14a98ff75dd619262f97a3537c7f38d7
Syafiq Basri <altsyafiq@gmail.com>
2022-10-25 22:23:30 +0800

    :sparkles: Added AuthGuard and updated router component imports

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import HomeView from '../views/HomeView.vue'
-import PublicLayoutVue from '@/layouts/PublicLayout.vue'
-import AuthLayoutVue from '@/layouts/AuthLayout.vue'
+import { useAuthStore } from '@/stores'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,58 +8,69 @@ const router = createRouter({
     {
       path: '/',
       name: 'public',
-      component: PublicLayoutVue,
+      component: () => import('@/layouts/PublicLayout.vue'),
       children: [
         {
           path: 'home',
           name: 'home',
-          component: HomeView
+          component: () => import('@/views/HomeView.vue')
         }
       ]
     },
     {
       path: '/auth',
       name: 'authentication',
-      component: AuthLayoutVue,
+      component: () => import('@/layouts/AuthLayout.vue'),
       children: [
         {
           path: 'login',
           name: 'login',
-          component: () => import('../views/auth/LoginView.vue')
+          component: () => import('@/views/auth/LoginView.vue')
         },
         {
           path: 'registration/customer',
           name: 'registerCustomer',
-          component: () => import('../views/auth/RegistrationView.vue')
+          component: () => import('@/views/auth/RegistrationView.vue')
         },
         {
           path: 'registration/vendor',
           name: 'registerVendor',
-          component: () => import('../views/auth/VendorRegistrationView.vue')
+          component: () => import('@/views/auth/VendorRegistrationView.vue')
         },
         {
           path: 'registration/resend-verification',
           name: 'resendVerification',
-          component: () => import('../views/auth/ResendVerificationView.vue')
+          component: () => import('@/views/auth/ResendVerificationView.vue')
         },
         {
           path: 'registration/verify',
           name: 'verify',
-          component: () => import('../views/auth/VerifyAccountView.vue')
+          component: () => import('@/views/auth/VerifyAccountView.vue')
         },
         {
           path: 'reset',
           name: 'reset',
-          component: () => import('../views/auth/ResetView.vue')
+          component: () => import('@/views/auth/ResetView.vue')
         },
         {
           path: 'reset/confirm',
           name: 'confirmReset',
-          component: () => import('../views/auth/ConfirmResetView.vue')
+          component: () => import('@/views/auth/ConfirmResetView.vue')
         },
       ]
     }
   ]
+})
+
+// AuthGuard
+router.beforeEach(async (to, from, next) => {
+  const authStore = useAuthStore()
+
+  if (authStore.$state.accessToken && to.path.startsWith('/auth')) {
+    return next({ path: '/home' })
+  }
+
+  return next()
 })
 
 export default router
