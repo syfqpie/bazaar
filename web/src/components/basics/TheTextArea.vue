9ebe95611043745d369beba7c017a9e9a64dd8b9
Syafiq Basri <altsyafiq@gmail.com>
2022-11-09 23:18:23 +0800

    :sparkles: Added TheCheckbox and TheTextArea components

---
+++ web/src/components/basics/TheTextArea.vue
@@ -0,0 +1,18 @@
+<template>
+    <textarea
+        class="mt-1 block w-full rounded-lg bg-gray-50
+        border border-gray-300 text-gray-900
+        text-sm p-2.5 focus:outline-none focus:border-indigo-700" >
+    </textarea>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'TheTextArea',
+    setup() {
+        return { }
+    }
+})
+</script>
