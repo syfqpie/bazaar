dc1bbaa610299c9fac3dcb2f1d35c5fb888506ce
Syafiq Basri <altsyafiq@gmail.com>
2022-11-09 23:00:18 +0800

    :sparkles: Added TheInput and TheRadio components

---
+++ web/src/components/basics/TheInput.vue
@@ -0,0 +1,18 @@
+<template>
+    <input
+        class="mt-1 block w-full rounded-lg bg-gray-50
+        border border-gray-300 text-gray-900
+        text-sm p-2.5 focus:outline-none focus:border-indigo-700
+        transition ease-out duration-300" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'TheInput',
+    setup() {
+        return { }
+    }
+})
+</script>
---
+++ web/src/components/basics/TheRadio.vue
@@ -0,0 +1,17 @@
+<template>
+    <input
+        type="radio"
+        class="w-4 h-4 accent-indigo-600 focus:ring-1 
+        focus:ring-indigo-700 transition-all ease-in duration-150" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'TheRadio',
+    setup() {
+        return { }
+    }
+})
+</script>
