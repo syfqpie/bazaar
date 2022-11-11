f63061fdff29db47270cbd3403b6e3eceb53c739
Syafiq Basri <altsyafiq@gmail.com>
2022-11-11 19:35:10 +0800

    :zap: Updated AccountProfile to use internal components; Removed unused codes; Added role

--- web/src/views/account/AccountProfile.vue
+++ web/src/views/account/AccountProfile.vue
@@ -11,97 +11,78 @@
             </p>
 
             <div class="grid grid-cols-2">
-                <form @submit.prevent>
+                <div>
                     <div class="mb-3">
                         <label class="text-sm text-gray-700">
                             Email
                         </label>
                         <input
                             type="email"
-                            class="mt-1 block w-full rounded-lg bg-gray-50
-                            border border-gray-300 text-gray-900
-                            text-sm p-2.5 focus:outline-none
-                            focus:shadow-outline focus:ring-gray-500
-                            focus:ring-1" />
+                            class="mt-1 block w-full rounded-lg disabled:bg-gray-50
+                            border disabled:border-gray-200 disabled:text-gray-400
+                            disabled:cursor-not-allowed text-sm p-2.5"
+                            :value="authStore.email"
+                            disabled />
                     </div>
 
                     <div class="mb-3">
                         <label class="text-sm text-gray-700">
-                            Name
+                            Role
                         </label>
-                        <input
-                            type="text"
-                            class="mt-1 block w-full rounded-lg bg-gray-50
-                            border border-gray-300 text-gray-900
-                            text-sm p-2.5 focus:outline-none
-                            focus:shadow-outline focus:ring-gray-500
-                            focus:ring-1" />
+                        
+                        <h5 class="mt-1 flex items-center text-sm font-extrabold">
+                            <span 
+                                class="text-medium font-semibold px-2.5 py-0.5 rounded"
+                                :class="{
+                                    'bg-gray-200 text-gray-900': authStore.userType === UserType.Admin,
+                                    'bg-pink-100 text-pink-800': authStore.userType === UserType.Vendor,
+                                    'bg-indigo-100 text-indigo-800': authStore.userType === UserType.Customer
+                                }">
+                                {{ authStore.getUserTypeNormal }}
+                            </span>
+                        </h5>
                     </div>
 
-                    <div class="mb-3">
-                        <label class="text-sm text-gray-700">
-                            Phone no.
-                        </label>
-                        <input
-                            type="text"
-                            class="mt-1 block w-full rounded-lg bg-gray-50
-                            border border-gray-300 text-gray-900
-                            text-sm p-2.5 focus:outline-none
-                            focus:shadow-outline focus:ring-gray-500
-                            focus:ring-1" />
-                    </div>
+                    <div v-if="authStore.userType !== UserType.Admin">
+                        <hr class="mt-4 mb-3 h-px bg-gray-200 border-0" />
+                        <ProfileVendor v-if="authStore.userType === UserType.Vendor" />
 
-                    <div class="mb-3">
-                        <label class="text-sm text-gray-700">
-                            Gender
-                        </label>
-                        <input
-                            type="text"
-                            class="mt-1 block w-full rounded-lg bg-gray-50
-                            border border-gray-300 text-gray-900
-                            text-sm p-2.5 focus:outline-none
-                            focus:shadow-outline focus:ring-gray-500
-                            focus:ring-1" />
+                        <ProfileCustomer v-else-if="authStore.userType === UserType.Customer" />
                     </div>
-
-                    <div class="mb-3">
-                        <label class="text-sm text-gray-700">
-                            Date of birth
-                        </label>
-                        <input
-                            type="date"
-                            class="mt-1 block w-full rounded-lg bg-gray-50
-                            border border-gray-300 text-gray-900
-                            text-sm p-2.5 focus:outline-none
-                            focus:shadow-outline focus:ring-gray-500
-                            focus:ring-1" />
-                    </div>
-
-                    <button
-                        class="block rounded-lg border border-transparent 
-                        bg-green-300 focus:hover:enabled:bg-green-500
-                        p-2.5 text-white enabled:bg-green-400
-                        focus:outline-none focus:ring-2 focus:ring-green-200
-                        font-medium text-sm">
-                        Save
-                    </button>
-                </form>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import { onMounted, defineComponent } from 'vue'
+import { defineComponent, onMounted } from 'vue'
+
+import { UserType } from '@/common/models/user.model'
+import ProfileVendor from '@/components/user-account/ProfileVendor.vue'
+import ProfileCustomer from '@/components/user-account/ProfileCustomer.vue'
+import { useAuthStore, useVendorStore } from '@/stores'
 
 export default defineComponent({
     name: 'AccountProfile',
     setup() {
+        // Services
+        const authStore = useAuthStore()
+        const vendorStore = useVendorStore()
+
         onMounted(() => {
             // console.log('Mounted AccountProfile')
         })
 
-        return {}
+        return {
+            authStore,
+            vendorStore,
+            UserType
+        }
+    },
+    components: {
+        ProfileVendor,
+        ProfileCustomer
     }
 })
 </script>
