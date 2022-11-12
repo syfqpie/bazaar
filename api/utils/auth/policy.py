bae0559e23a37865e83eb05c2dde66a0f5904fd2
Syafiq Basri <altsyafiq@gmail.com>
2022-11-12 20:29:17 +0800

    :sparkles: Added is_vendor, is_customer to BaseAccessPolicy

--- api/utils/auth/policy.py
+++ api/utils/auth/policy.py
@@ -9,7 +9,19 @@ class BaseAccessPolicy(AccessPolicy):
     """
 
     def is_admin(self, request, view, action) -> bool:
-        """ Check if customer user and request user is same or admin """
+        """ Check if request user is admin """
         
         user = request.user
         return user.user_type == UserType.ADMIN
+    
+    def is_vendor(self, request, view, action) -> bool:
+        """ Check if request user is vendor """
+        
+        user = request.user
+        return user.user_type == UserType.VENDOR
+
+    def is_customer(self, request, view, action) -> bool:
+        """ Check if request user is customer """
+        
+        user = request.user
+        return user.user_type == UserType.CUSTOMER
