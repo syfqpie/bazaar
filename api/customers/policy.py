d759226cb713358c099f7482f88a71df4190ffbe
Syafiq Basri <altsyafiq@gmail.com>
2022-11-12 20:30:32 +0800

    :sparkles: Added CustomerAddressAccessPolicy

--- api/customers/policy.py
+++ api/customers/policy.py
@@ -44,3 +44,53 @@ class CustomerAccessPolicy(BaseAccessPolicy):
             return queryset.none()
         
         return queryset
+
+
+class CustomerAddressAccessPolicy(BaseAccessPolicy):
+    """
+    Access policy for CustomerAddressViewSet
+    """
+
+    statements = [
+        {
+            'action': ['create'],
+            'principal': ['authenticated'],
+            'effect': 'allow',
+            'condition': 'is_customer'
+        },
+        {
+            'action': ['list'],
+            'principal': ['authenticated'],
+            'effect': 'allow'
+        },
+        {
+            'action': ['partial_update'],
+            'principal': ['authenticated'],
+            'effect': 'allow',
+            'condition': 'is_address_owner'
+        },
+        {
+            'action': ['retrieve'],
+            'principal': ['authenticated'],
+            'effect': 'allow',
+            'condition_expression': 'is_address_owner or is_admin'
+        }
+    ]
+
+    def is_address_owner(self, request, view, action) -> bool:
+        """ Check if addresss owner and request user is same """
+
+        address = view.get_object()
+        return request.user == address.customer.user
+
+    @classmethod
+    def scope_queryset(cls, request, queryset):
+        """ Filter queryset according to roles """
+        user = request.user
+
+        if user.user_type == UserType.CUSTOMER:
+            return queryset.filter(customer=user.related_customer)
+        elif user.user_type == UserType.VENDOR:
+            return queryset.none()
+        
+        return queryset
