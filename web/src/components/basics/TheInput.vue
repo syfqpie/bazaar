4830f25e66fff41a7d41667dca85d90bfe9d3fb2
Syafiq Basri <altsyafiq@gmail.com>
2022-11-12 22:27:22 +0800

    :art: Updated to add disabled classes

--- web/src/components/basics/TheInput.vue
+++ web/src/components/basics/TheInput.vue
@@ -4,7 +4,8 @@
         border border-gray-300 text-gray-900
         text-sm p-2.5 focus:outline focus:outline-2
         focus:outline-indigo-200 focus:border-indigo-700
-        transition ease-out duration-300"
+        transition ease-out duration-300 disabled:text-gray-500
+        disabled:bg-gray-100 disabled:cursor-not-allowed"
         :class="$attrs.class"
         type="{{ $attrs.type }}"
         :value="modelValue" 
