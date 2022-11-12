6dc43d838e1bd3d7dc4ef33149b00e0b84d0dbab
Syafiq Basri <altsyafiq@gmail.com>
2022-11-12 10:32:23 +0800

    :sparkles: Added basic component enums

---
+++ web/src/common/utility/basic.model.ts
@@ -0,0 +1,32 @@
+/** Enum for basic color */
+export enum BasicColor {
+    /** Default color */
+    DEFAULT = 'indigo',
+
+    /** Success color */
+    SUCCESS = 'green',
+
+    /** Info color */
+    INFO = 'sky',
+
+    /** Warning color */
+    WARNING = 'yellow',
+
+    /** Danger color */
+    DANGER = 'red'
+}
+
+/** Enum for modal size */
+export enum ModalSize {
+    /** Small modal size */
+    SM = 'sm',
+
+    /** Medium modal size */
+    MD = 'md',
+
+    /** Large modal size */
+    LG = 'lg',
+
+    /** Extra large modal size */
+    XL = 'xl'
+}
