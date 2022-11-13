6b262ccc0309369482e95def19ff02ee549b70d3
Syafiq Basri <altsyafiq@gmail.com>
2022-11-13 15:51:40 +0800

    :sparkles: Added destroy to CustomerAddressAccessPolicy and removed destroy from CustomerAccessPolicy

--- api/customers/policy.py
+++ api/customers/policy.py
@@ -14,7 +14,7 @@ class CustomerAccessPolicy(BaseAccessPolicy):
             'effect': 'allow'
         },
         {
-            'action': ['partial_update', 'destroy'],
+            'action': ['partial_update'],
             'principal': ['authenticated'],
             'effect': 'allow',
             'condition': 'is_customer_user'
@@ -64,7 +64,7 @@ class CustomerAddressAccessPolicy(BaseAccessPolicy):
             'effect': 'allow'
         },
         {
-            'action': ['partial_update'],
+            'action': ['partial_update', 'destroy'],
             'principal': ['authenticated'],
             'effect': 'allow',
             'condition': 'is_address_owner'
