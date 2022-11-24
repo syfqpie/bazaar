b09d9bca67962d795436ecaef98a1dcfd62c1497
Dev-R <rabimhd@icloud.com>
2022-11-24 22:03:35 +0800

    :art: Added Product Listing scaffold

---
+++ web/src/views/ProductListingView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="w-full">
+        <p>Product Listing</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { onMounted, defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'ProductListing',
+    setup() {
+        onMounted(() => {
+            // console.log('Mounted ProductListing')
+        })
+
+        return {}
+    }
+})
+</script>
