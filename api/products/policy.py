795de4bf12cbd08ad108c2c5be994770cdf39b0c
Syafiq Basri <altsyafiq@gmail.com>
2022-12-05 20:44:20 +0800

    :construction: Added ProductAccessPolicy

--- api/products/policy.py
+++ api/products/policy.py
@@ -30,5 +30,43 @@ class CategoryAccessPolicy(BaseAccessPolicy):
     @classmethod
     def scope_queryset(cls, request, queryset):
         """ Filter queryset according to roles """
+
+        return queryset
+
+
+class ProductAccessPolicy(BaseAccessPolicy):
+    """
+    Access policy for ProductViewSet
+    """
+
+    statements = [
+        {
+            'action': ['list', 'retrieve'],
+            'principal': '*',
+            'effect': 'allow'
+        },
+        {
+            'action': ['create'],
+            'principal': ['authenticated'],
+            'effect': 'allow',
+            'condition': 'is_vendor'
+        },
+        {
+            'action': ['partial_update', 'destroy'],
+            'principal': ['authenticated'],
+            'effect': 'allow',
+            'condition_expression': 'is_vendor and is_owner'
+        }
+    ]
+
+    def is_owner(self, request, view, action) -> bool:
+        """ Check if vendor user and request user is same """
+
+        product = view.get_object()
+        return request.user == product.vendor.user
+
+    @classmethod
+    def scope_queryset(cls, request, queryset):
+        """ Filter queryset according to roles """
         
         return queryset
