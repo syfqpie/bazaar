5d0b1d6248cc7c2f165d8d7138c008ffd4a0499b
Syafiq Basri <altsyafiq@gmail.com>
2022-12-05 12:31:09 +0800

    :construction: Added Base serializers for Product, Category, Variant, Inventory, Media models

---
+++ api/products/serializers.py
@@ -0,0 +1,69 @@
+"""
+Product's app serializers
+"""
+from rest_framework import serializers
+
+from .models import Product, Category, Variant, Inventory, Media
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    """
+    Base serializer for Product model
+    """
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+        read_only_fields = [
+            'is_active',
+            'rating'
+        ]
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    """
+    Base serializer for Category model
+    """
+
+    class Meta:
+        model = Category
+        fields = '__all__'
+        read_only_fields = [
+            'is_active'
+        ]
+
+
+class VariantSerializer(serializers.ModelSerializer):
+    """
+    Base serializer for Variant model
+    """
+
+    class Meta:
+        model = Variant
+        fields = '__all__'
+        read_only_fields = [
+            'is_active'
+        ]
+
+
+class InventorySerializer(serializers.ModelSerializer):
+    """
+    Base serializer for Inventory model
+    """
+
+    class Meta:
+        model = Inventory
+        fields = '__all__'
+        read_only_fields = [
+            'is_active'
+        ]
+
+
+class MediaSerializer(serializers.ModelSerializer):
+    """
+    Base serializer for Media model
+    """
+
+    class Meta:
+        model = Media
+        fields = '__all__'
