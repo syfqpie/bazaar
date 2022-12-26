70d53784d964af95945e3551c1c2b25d1dcdd652
Syafiq Basri <altsyafiq@gmail.com>
2022-12-26 23:55:48 +0800

    :construction: Added LayoutVendor compoenent

---
+++ web/src/layouts/LayoutVendor.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="min-h-screen bg-gray-100">
+        <div class="flex">
+            <SidebarVendor class="flex-none sticky top-0 h-screen" />
+            <div class="flex-auto bg-white">
+                <RouterView />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { onMounted, defineComponent } from 'vue'
+
+import SidebarVendor from '@/components/sidebars/SidebarVendor.vue'
+
+export default defineComponent({
+    name: 'LayoutVendor',
+    setup() {
+        onMounted(() => {
+            // console.log('Mounted LayoutVendor')
+        })
+
+        return {}
+    },
+    components: {
+        SidebarVendor
+    }
+})
+</script>
