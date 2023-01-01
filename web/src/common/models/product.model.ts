2b5af7ea4e3f5b9722239daf3efcf111806d8e9f
Syafiq Basri <altsyafiq@gmail.com>
2023-01-01 19:25:10 +0800

    :construction: Moved and updated VariantInput, added VariantBase and Variant; Updated ProductInput and added ProductBase and Product

--- web/src/common/models/product.model.ts
+++ web/src/common/models/product.model.ts
@@ -1,32 +1,16 @@
-/** Base interface for variant input */
-export interface VariantInput {
-    /** Variant's name */
-    name: string | null,
+import type { VariantInput } from './variant.model';
 
-    /** Variant's price */
-    price: number | null,
-
-    /** Variant's quantity */
-    quantity: number | null,
-
-    /** Variant's sku no. */
-    sku: string | null,
-
-    /** Variant's  weight */
-    weight: number | null,
-
-    /** Variant's quantity limit for customer */
-    customerQuantityLimit: number | null
-}
-
-/** Base interface for product input  */
-export interface ProductInput {
+/** Base interface for Product  */
+export interface ProductBase {
     /** Product's name */
     name: string | null,
 
     /** Product's description */
-    description: string | null,
+    description: string | null
+}
 
+/** Input interface for Product */
+export interface ProductInput extends ProductBase {
     /** Product's categories */
     category: number[],
 
@@ -35,4 +19,34 @@ export interface ProductInput {
 
     /** Product's variants */
     variants: VariantInput[]
+}
+
+/** Core interface for Product */
+export interface Product extends ProductBase {
+    /** Product's db id */
+    id: number,
+
+    /** Product's name */
+    name: string,
+
+    /** Product's slug */
+    slug: string,
+
+    /** Product's description */
+    description: string,
+
+    /** Product's categories */
+    category: number[],
+
+    /** Product's rating */
+    rating: number | null,
+
+    /** Is product active? */
+    isActive: boolean,
+
+    /** Product's created datetime */
+    createdAt: string,
+
+    /** Product's last modified datetime */
+    lastModifiedAt: string
 }
