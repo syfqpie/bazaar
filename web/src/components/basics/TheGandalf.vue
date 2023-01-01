85cd2654f8d5090aa308bb7694507d25358627b5
Syafiq Basri <altsyafiq@gmail.com>
2023-01-01 11:46:51 +0800

    :construction: Updated TheGandalf icon  styles

--- web/src/components/basics/TheGandalf.vue
+++ web/src/components/basics/TheGandalf.vue
@@ -274,7 +274,7 @@ export default defineComponent({
 	background-position: center center;
 }
 
-.wizard .wizard-step.is-visited:not(.is-current):not(.is-skippable) .wizard-step-counter:after {
+.wizard .wizard-step.is-visited:not(.is-current, .is-completed) .wizard-step-counter:after {
 	background-color: var(--wizard-step);
 	background-size: 0.55rem;
 	/* Refered to @fortawesome/fontawesome-free/svgs/solid/triangle-exclamation.svg */
@@ -288,7 +288,7 @@ export default defineComponent({
 	background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 576 512'%3e%3cpath fill='%236366f1' d='M234.7 42.7L197 56.8c-3 1.1-5 4-5 7.2s2 6.1 5 7.2l37.7 14.1L248.8 123c1.1 3 4 5 7.2 5s6.1-2 7.2-5l14.1-37.7L315 71.2c3-1.1 5-4 5-7.2s-2-6.1-5-7.2L277.3 42.7 263.2 5c-1.1-3-4-5-7.2-5s-6.1 2-7.2 5L234.7 42.7zM46.1 395.4c-18.7 18.7-18.7 49.1 0 67.9l34.6 34.6c18.7 18.7 49.1 18.7 67.9 0L529.9 116.5c18.7-18.7 18.7-49.1 0-67.9L495.3 14.1c-18.7-18.7-49.1-18.7-67.9 0L46.1 395.4zM484.6 82.6l-105 105-23.3-23.3 105-105 23.3 23.3zM7.5 117.2C3 118.9 0 123.2 0 128s3 9.1 7.5 10.8L64 160l21.2 56.5c1.7 4.5 6 7.5 10.8 7.5s9.1-3 10.8-7.5L128 160l56.5-21.2c4.5-1.7 7.5-6 7.5-10.8s-3-9.1-7.5-10.8L128 96 106.8 39.5C105.1 35 100.8 32 96 32s-9.1 3-10.8 7.5L64 96 7.5 117.2zm352 256c-4.5 1.7-7.5 6-7.5 10.8s3 9.1 7.5 10.8L416 416l21.2 56.5c1.7 4.5 6 7.5 10.8 7.5s9.1-3 10.8-7.5L480 416l56.5-21.2c4.5-1.7 7.5-6 7.5-10.8s-3-9.1-7.5-10.8L480 352l-21.2-56.5c-1.7-4.5-6-7.5-10.8-7.5s-9.1 3-10.8 7.5L416 352l-56.5 21.2z'/%3e%3c/svg%3e");
 }
 
-.wizard .wizard-step:is(.is-completed, .is-skippable):not(.is-current) .wizard-step-counter:after {
+.wizard .wizard-step:is(.is-completed):not(.is-current) .wizard-step-counter:after {
 	background-color: var(--wizard-step);
 	background-size: 0.55rem;
 	/* Refered to @fortawesome/fontawesome-free/svgs/solid/check.svg */
