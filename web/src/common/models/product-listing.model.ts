c1de001b63c4969b2269332035c31984a91661ef
Dev-R <rabimhd@icloud.com>
2023-01-01 20:25:55 +0800

    :sparkles: Added product image carousel

---
+++ web/src/common/models/product-listing.model.ts
@@ -0,0 +1,9 @@
+/** Interface for Product-Listing */
+export interface ProductListingImage {
+    /** Image index of the image */
+    index: number
+    /** Image URL */
+    imageUrl: string
+    /** image Title/name */
+    title?: string
+}
