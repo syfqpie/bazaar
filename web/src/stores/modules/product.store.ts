adc792ca1be18a73229bab763eef343bab9cdaf4
Syafiq Basri <altsyafiq@gmail.com>
2023-01-06 22:56:41 +0800

    :construction: Added retrieve, list; Updated create

--- web/src/stores/modules/product.store.ts
+++ web/src/stores/modules/product.store.ts
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 
 import { APIService } from '@/common/api'
-import type { ProductInput } from '@/common/models/product.model'
+import type { Product, ProductInput } from '@/common/models/product.model'
 import type { Variant, VariantInput } from '@/common/models/variant.model'
 import { V1_PREFIX } from './prefix'
 
@@ -34,13 +34,41 @@ export const useProductStore = defineStore('product', {
             }
         },
         /** Create new product */
-        create(payload: ProductInput): Promise<Variant> {
+        create(payload: ProductInput): Promise<Product> {
             return new Promise((resolve, reject) => {
                 APIService.post(BASE_ENDPOINT, payload)
                     .then(({ data }) => {
                         resolve(data)
 
                         this.$state.product = data
+                    })
+                    .catch(err => {
+                        reject(err)
+                    })
+            })
+        },
+        /** Retrieve a product */
+        retrieve(id: number): Promise<Product> {
+            return new Promise((resolve, reject) => {
+                APIService.get(`${ BASE_ENDPOINT }/${ id }`)
+                    .then(({ data }) => {
+                        resolve(data)
+
+                        this.$state.product = data
+                    })
+                    .catch(err => {
+                        reject(err)
+                    })
+            })
+        },
+        /** List products */
+        list(): Promise<Product> {
+            return new Promise((resolve, reject) => {
+                APIService.get(BASE_ENDPOINT)
+                    .then(({ data }) => {
+                        resolve(data)
+
+                        this.$state.products = data
                     })
                     .catch(err => {
                         reject(err)
